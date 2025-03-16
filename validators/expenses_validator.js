5985811568b0f465003a372e4dc4d351eaec882f
gloria larbi <Miss-Gloria>
2025-03-16 21:10:18 +0000

    worked on the validators

--- validators/expenses_validator.js
+++ validators/expenses_validator.js
@@ -3,7 +3,7 @@ import Joi from "joi";
 export const registerValidator =  Joi.object({
     username: Joi.string().min(3).max(30).required(),
     email: Joi.string().email().required(),
-    password: Joi.string().min(8).required
+    password: Joi.string().min(8).pattern(new RegExp("^(?=.*[A-Z])(?=.*[a-z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$")).message("Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character.").required()
 });
 
 export const loginValidator = Joi.object({
